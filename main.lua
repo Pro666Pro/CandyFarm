a3e43178a3507d7a91a32c5d3057c5484ae0c3dd
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-17 11:04:18 +0900

    Update and rename .gitignore to main.lua

---
+++ main.lua
@@ -0,0 +1,19 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+repeat task.wait() until game.Players.LocalPlayer.Character ~= nil and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+if game.Players.LocalPlayer.Character:FindFirstChild("entered") == nil then
+    repeat task.wait()
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 0)
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 1)
+    until game.Players.LocalPlayer.Character:FindFirstChild("entered")
+end
+for i, v in pairs(game.Workspace.CandyCorns:GetChildren()) do
+                if game.Players.LocalPlayer.Character:FindFirstChild("Head") and v:FindFirstChildWhichIsA("TouchTransmitter") then
+                    firetouchinterest(game.Players.LocalPlayer.Character.Head, v, 0)
+                    firetouchinterest(game.Players.LocalPlayer.Character.Head, v, 1)
+                end
+            end
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
